372768fde5fc125334c614b87b50e39b52ddd805
sahitya-priya-d <dpriya@galaxe.com>
2024-02-27 23:20:26 +0530

    Added UI and App

---
+++ app/GxHospitalManagement_SB/src/test/java/com/galaxe/GxHospitalManagement/GxHospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galaxe.GxHospitalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GxHospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
